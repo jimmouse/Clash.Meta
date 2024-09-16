f9c1ca55116f1bfd68377264ba2a27a30b11d688
Snail Mail <>
2024-09-16 10:50:14 +0800

    first

--- test/dns_test.go
+++ test/dns_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -14,7 +13,6 @@ func exchange(address, domain string, tp uint16) ([]dns.RR, error) {
 	client := dns.Client{}
 	query := &dns.Msg{}
 	query.SetQuestion(dns.Fqdn(domain), tp)
-	fmt.Println("aa")
 	r, _, err := client.Exchange(query, address)
 	if err != nil {
 		return nil, err
